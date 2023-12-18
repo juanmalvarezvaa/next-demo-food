7bf8c4e226bbc986eaef86c7722db3a615b3a960
Juan <juan.alvarez.vaa@gmail.com>
2023-12-18 13:33:42 -0300

    Add some base links and pages

--- app/page.js
+++ app/page.js
@@ -1,9 +1,21 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main>
-      <h1 style={{ color: 'white', textAlign: 'center' }}>
-        Starting...
-      </h1>
+      <h1 style={{ color: "white", textAlign: "center" }}>Starting...</h1>
+      <p>
+        <Link href="/community">Community</Link>
+      </p>
+      <p>
+        <Link href="/meals">Meals</Link>
+      </p>
+      <p>
+        <Link href="/meals/share">Meals Share</Link>
+      </p>
+      <p>
+        <Link href="/meals/123">Meals 123</Link>
+      </p>
     </main>
   );
 }
