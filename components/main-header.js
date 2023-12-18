33423b2adf8df1cdfe2c4e357d4c6bc22b190652
Juan <juan.alvarez.vaa@gmail.com>
2023-12-18 14:38:38 -0300

    Styling Main Header

--- components/main-header.js
+++ components/main-header.js
@@ -1,16 +1,17 @@
 import Link from "next/link";
 
+import styles from "./main-header.module.css";
 import logoImg from "@/assets/logo.png";
 
 export default function MainHeader() {
   return (
-    <header>
-      <Link href="/">
+    <header className={styles.header}>
+      <Link href="/" className={styles.logo}>
         <img src={logoImg.src} alt="Next Demo Food!" />
         Next Demo Food App
       </Link>
 
-      <nav>
+      <nav className={styles.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meals</Link>
