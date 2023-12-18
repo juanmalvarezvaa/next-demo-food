e7209330e7ed242138749074338b3d9301f625bb
Juan <juan.alvarez.vaa@gmail.com>
2023-12-18 14:48:25 -0300

    Refactor Main Header

---
+++ components/main-header/main-header.js
@@ -0,0 +1,31 @@
+import Link from "next/link";
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+
+import styles from "./main-header.module.css";
+import logoImg from "@/assets/logo.png";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={styles.header}>
+        <Link href="/" className={styles.logo}>
+          <Image src={logoImg} alt="Next Demo Food!" priority />
+          Next Demo Food App
+        </Link>
+
+        <nav className={styles.nav}>
+          <ul>
+            <li>
+              <Link href="/meals">Browse Meals</Link>
+            </li>
+            <li>
+              <Link href="/community">Food Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
