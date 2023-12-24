566d4ee4976dea5e2530d87337cf2185bba26c49
Juan <juan.alvarez.vaa@gmail.com>
2023-12-24 17:49:30 -0300

    Add Share page

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,51 @@
+import styles from "./page.module.css";
+
+import ImagePicker from "@/components/meals/image-picker";
+
 export default function MealsSharePage() {
-  return <h1>Share Page</h1>;
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Share your <span className={styles.highlight}>favorite recipe!</span>
+        </h1>
+        <p>Fill the form below to share your favorite recipe with the world!</p>
+      </header>
+      <main className={styles.main}>
+        <form className={styles.form}>
+          <div className={styles.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Your Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          <ImagePicker />
+          <p className={styles.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+      </main>
+    </>
+  );
 }
