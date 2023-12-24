be0fb30d6fcbce87acf7aa4803c3317d4d74ec10
Juan <juan.alvarez.vaa@gmail.com>
2023-12-24 16:54:51 -0300

    Add Not Found default page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <main className="not-found">
+      <h1>Page not found</h1>
+      <p>Sorry, the page you requested could not be found.</p>
+    </main>
+  );
+}
