87edfd466aeecdef3d08a5b781d2dad5b15792c7
Juan <juan.alvarez.vaa@gmail.com>
2023-12-24 17:23:21 -0300

    Add /meals/mealId page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,3 +1,33 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+import { getMeal } from "@/lib/meals";
+
 export default function MealDetails({ params }) {
-  return <h1>Meal Id Page: {params.slug}</h1>;
+  const meal = getMeal(params.slug);
+
+  meal.instructions = meal.instructions.replace(/\n/g, "<br />");
+
+  return (
+    <>
+      <header className={styles.header}>
+        <div className={styles.image}>
+          <Image fill src={meal.image} alt={meal.title} />
+        </div>
+        <div className={styles.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={styles.creator}>
+            by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={styles.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={styles.instructions}
+          dangerouslySetInnerHTML={{ __html: meal.instructions }}
+        ></p>
+      </main>
+    </>
+  );
 }
