308ee20a281fcb1113fc737b45f72d33a7f511bc
Juan <juan.alvarez.vaa@gmail.com>
2023-12-27 16:24:16 -0300

    Will add feedback to button when submiting
    I moved it to a component to use useFormStatus and use client directive

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -2,6 +2,7 @@ import styles from "./page.module.css";
 
 import ImagePicker from "@/components/meals/image-picker";
 import { shareMeal } from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function MealsSharePage() {
   return (
@@ -43,7 +44,7 @@ export default function MealsSharePage() {
           </p>
           <ImagePicker label="Your image" name="image" />
           <p className={styles.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
