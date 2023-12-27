40f3d8ee35ce9f53f0fced86bf77e1e779697f98
Juan <juan.alvarez.vaa@gmail.com>
2023-12-27 16:39:41 -0300

    Add useFormState hook
    This will handle Form status, and be able to return objects from the form submit function

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,3 +1,6 @@
+"use client";
+
+import { useFormState } from "react-dom";
 import styles from "./page.module.css";
 
 import ImagePicker from "@/components/meals/image-picker";
@@ -5,6 +8,11 @@ import { shareMeal } from "@/lib/actions";
 import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function MealsSharePage() {
+  // Hook by react to control the state of a Form. fn -> The one called by the Form. initialState -> The initial state of the Form (to handle errors).
+  // https://react.dev/reference/react-dom/hooks/useFormState
+  // const [state, formAction] = useFormState(fn, initialState);
+  const [state, formAction] = useFormState(shareMeal, { message: null });
+
   return (
     <>
       <header className={styles.header}>
@@ -14,7 +22,7 @@ export default function MealsSharePage() {
         <p>Fill the form below to share your favorite recipe with the world!</p>
       </header>
       <main className={styles.main}>
-        <form className={styles.form} action={shareMeal}>
+        <form className={styles.form} action={formAction}>
           <div className={styles.row}>
             <p>
               <label htmlFor="name">Your name</label>
@@ -43,6 +51,7 @@ export default function MealsSharePage() {
             ></textarea>
           </p>
           <ImagePicker label="Your image" name="image" />
+          {state.message && <p>{state.message}</p>}
           <p className={styles.actions}>
             <MealsFormSubmit />
           </p>
