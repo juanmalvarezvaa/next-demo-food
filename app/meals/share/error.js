c69568348d50a0098a36923c92ea5360ffd5367d
Juan <juan.alvarez.vaa@gmail.com>
2023-12-27 16:30:19 -0300

    Add backend validaor and custom error when submitting a new meal.

---
+++ app/meals/share/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h1>Invalid Input</h1>
+      <p>Failed to create Meal.</p>
+    </main>
+  );
+}
